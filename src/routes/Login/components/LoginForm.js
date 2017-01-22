7d4e92716b61ebc5aaa56a8728a50345f8748d0b
Arleigh Dickerson <a.dickerson@aeapubs.org>
2017-01-22 17:01:52 -0500

    refactors LoginForm to pure component

--- src/routes/Login/components/LoginForm.js
+++ src/routes/Login/components/LoginForm.js
@@ -20,39 +20,38 @@ const constraints = {
 
 const validate = require('app/util/validate')(constraints)
 
-class LoginForm extends React.Component {
-  render() {
-    const {username, password, handleSubmit, pristine, submitting} = this.props;
-    return (
-      <form
-        className="m-x-auto text-center app-login-form"
-        role="form"
-        method="POST"
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (this.props.valid) {
-            handleSubmit(username, password);
-          }
-        }}>
-        <Link to="/" className="app-brand m-b-lg"
-              style={{width: '100%', textDecoration: 'none', fontFamily: 'Roboto-Bold'}}>
-          <h1>[Dank]</h1>
-        </Link>
-        <Field component={Input} placeholder='username' name='username' value={username}/>
-        <Field component={Input} placeholder='password' name='password' type='password' value={password}
-               componentClasses={{'FormGroup': FormGroup}}/>
+function LoginForm(props) {
+  const {username, password, handleSubmit, pristine, submitting} = props;
+  return (
+    <form
+      className="m-x-auto text-center app-login-form"
+      role="form"
+      method="POST"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (props.valid) {
+          handleSubmit(username, password);
+        }
+      }}>
+      <Link to="/" className="app-brand m-b-lg"
+            style={{width: '100%', textDecoration: 'none', fontFamily: 'Roboto-Bold'}}>
+        <h1>[Dank]</h1>
+      </Link>
+      <Field component={Input} placeholder='username' name='username' value={username}/>
+      <Field component={Input} placeholder='password' name='password' type='password' value={password}
+             componentClasses={{'FormGroup': FormGroup}}
+      />
 
-        <div className="m-b-lg">
-          <button className="btn btn-primary">Log In</button>
-          <button className="btn btn-default">Sign up</button>
-        </div>
+      <div className="m-b-lg">
+        <button className="btn btn-primary">Log In</button>
+        <button className="btn btn-default">Sign up</button>
+      </div>
 
-        <footer className="screen-login">
-          <a href="#" className="text-muted">Forgot password</a>
-        </footer>
-      </form>
-    )
-  }
+      <footer className="screen-login">
+        <a href="#" className="text-muted">Forgot password</a>
+      </footer>
+    </form>
+  )
 }
 
 export default reduxForm({
