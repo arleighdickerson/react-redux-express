433145d1b116b4f11991d384af7d74957e36d9e5
Arleigh Dickerson <a.dickerson@aeapubs.org>
2017-01-21 22:45:42 -0500

    adds site routes to express

--- server/routes/site.routes.js
+++ server/routes/site.routes.js
@@ -1,4 +1,6 @@
 module.exports = (app) => {
   app.route('/').get((req, res) => res.isomorphic({}))
   app.route('/counter').get((req, res) => res.isomorphic({}))
+  app.route('/about').get((req, res) => res.isomorphic({}))
+  app.route('/login').get((req, res) => res.isomorphic({}))
 }
