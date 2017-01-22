12d3142a7bc41bf27c99246d76ff216e447a799f
Arleigh Dickerson <a.dickerson@aeapubs.org>
2017-01-22 08:54:59 -0500

    factory-annotated classes always return the same instance when resolved

--- server/di/factory.js
+++ server/di/factory.js
@@ -1,15 +1,31 @@
 const _factories = new WeakMap()
 
-export default function factory(method = 'factory') {
+/**
+ *
+ * @param method the factory method name
+ * @returns {decorator}
+ */
+export default function factory(method = 'getObject') {
   return function decorator(Clazz) {
+    if (!Clazz.prototype.hasOwnProperty(method)) {
+      throw new Error(
+        `Factory method '${method}' was not found on factory-annotated object`
+      )
+    }
     _factories.set(Clazz, method)
-  };
+  }
 }
 
 export function hasFactory(clazz) {
   return _factories.has(clazz)
 }
 
-export function invokeFactory(clazz,instance){
-  return instance[_factories.get(clazz)]()
+const _flyweights = new WeakMap()
+
+export function invokeFactory(clazz, instance) {
+  if (!_flyweights.has(instance)) {
+    const flyweight = instance[_factories.get(clazz)]()
+    _flyweights.set(instance, flyweight);
+  }
+  return _flyweights.get(instance)
 }
