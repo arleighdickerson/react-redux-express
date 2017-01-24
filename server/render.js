60266e816adc4b402f54b04bebf6dc908be33229
Arleigh Dickerson <a.dickerson@aeapubs.org>
2017-01-23 21:07:48 -0500

    optimizes renderer

--- server/render.js
+++ server/render.js
@@ -28,61 +28,106 @@ const AppContainer = require('../src/containers/AppContainer').default
 const createStore = require('../src/store/createStore').default
 const createRoutes = require('../src/routes/index').default
 
-function renderIsomorphic(req, res, next, state = {}) {
-  const pathname = req.path
-  const initialState = {
-    routing: { //initialize router location
-      location: {
-        pathname
-      }
-    },
-    ...state
+class RenderStrategy {
+  getTemplate() {
+    return new Promise((resolve, reject) => this.filesystem
+      .readFile(this.filename, 'utf-8', (err, content) => err
+        ? reject(err)
+        : resolve(content)
+      )
+    )
   }
 
-  //default to client-only rendering
-  let component = ''
-  if (config.globals.__PROD__) {
-    global.___INITIAL_STATE__ = initialState
-    const history = ReactRouter.createMemoryHistory(pathname)
-    const store = createStore(history, initialState)
-    const routes = createRoutes(store)
-    //render on server in production
-    component = ReactDOMServer.renderToString(
-      React.createElement(AppContainer, {store, routes, history})
-    )
-    delete global.___INITIAL_STATE__
-  }
-  const handleFile = (err, result) => {
-    if (err) {
-      return next(err)
+  normalizeState(req, initialState) {
+    const pathname = req.path
+    return {
+      routing: { //initialize router location
+        location: {
+          pathname
+        }
+      },
+      ...initialState
     }
-    _.templateSettings.interpolate = /{{([\s\S]+?)}}/g //<-- NEEDS to be right before response send
+  }
+
+  interpolate(result, {component, initialState}) {
+    _.templateSettings.interpolate = /{{([\s\S]+?)}}/g
+    return _.template(result)({
+      component,
+      initialState: JSON.stringify(initialState)
+    })
+  }
+
+  send(res, content) {
     res
       .set('content-type', 'text/html')
       .status(200)
-      .send(_.template(result)({
-        component,
-        initialState: JSON.stringify(initialState)
-      }))
+      .send(content)
       .end()
   }
-  if (config.globals.__PROD__) {
-    const filename = path.join(config.utils_paths.dist('index.html'))
-    fs.readFile(filename, 'utf-8', handleFile)
-  } else {
-    const filename = path.join(compiler.outputPath, 'index.html')
-    compiler.outputFileSystem.readFile(filename, (err, result) => {
-      if (err) {
-        return next(
-          "The template could not be retrieved for interpolation. Has the compiler finished running?"
-        )
-      }
-      handleFile(err, result)
-    })
+
+  render(req, res, next, initialState) {
+    const component = this.getComponent(initialState)
+    const self = this
+    this.getTemplate()
+      .then(result => {
+        const content = self.interpolate(result, {initialState, component})
+        self.send(res, content)
+      }).catch(err => next(err))
   }
 }
 
+class ClientOnlyStrategy extends RenderStrategy {
+  get filename() {
+    return path.join(compiler.outputPath, 'index.html')
+  }
+
+  get filesystem() {
+    return compiler.outputFileSystem
+  }
+
+  getComponent() {
+    return ''
+  }
+}
+
+class IsomorphicStrategy extends RenderStrategy {
+  constructor() {
+    super()
+    this.filesystem = fs
+    this.filename = path.join(config.utils_paths.dist('index.html'))
+    const self = this
+    this.template = super.getTemplate().then(result => {
+      self.template = new Promise(resolve => resolve(result))
+      return result
+    })
+  }
+
+  getTemplate() {
+    return this.template
+  }
+
+  getComponent(initialState) {
+    try {
+      global.___INITIAL_STATE__ = initialState
+      const history = ReactRouter.createMemoryHistory(initialState.routing.location.pathname)
+      const store = createStore(history, initialState)
+      const routes = createRoutes(store)
+      return ReactDOMServer.renderToString(
+        React.createElement(AppContainer, {store, routes, history})
+      )
+    } finally {
+      delete global.___INITIAL_STATE__
+    }
+  }
+}
+
+
+const STRATEGY = config.globals.__PROD__
+  ? new IsomorphicStrategy()
+  : new ClientOnlyStrategy()
+
 module.exports = app => app.use((req, res, next) => {
-  res.isomorphic = (initialState = {}) => renderIsomorphic(req, res, next, initialState)
+  res.isomorphic = (initialState = {}) => STRATEGY.render(req, res, next, STRATEGY.normalizeState(req, initialState))
   next()
 })
