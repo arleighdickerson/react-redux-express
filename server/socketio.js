2d6a75c97f37298dd0965b995d60a6cb9ab3ec24
Arleigh Dickerson <a.dickerson@aeapubs.org>
2017-01-25 17:48:57 -0500

    adds server socketio audio handler

--- server/socketio.js
+++ server/socketio.js
@@ -1,11 +1,10 @@
-// Invoke 'strict' JavaScript mode
-'use strict';
 const debug = require('debug')('app:server:socketio')
+const ss = require('socket.io-stream')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
 
-// Load the module dependencies
-var config = require('./config'),
-  cookieParser = require('cookie-parser'),
-  passport = require('passport');
+const config = require('./config')
+const createStream = require('app/util/create-stream')
 
 // Define the Socket.io configuration method
 module.exports = function (server, io, mongoStore) {
@@ -15,7 +14,7 @@ module.exports = function (server, io, mongoStore) {
     // Use the 'cookie-parser' module to parse the request cookies
     cookieParser(config.sessionSecret)(socket.request, {}, function (err) {
       // Get the session id from the request cookies
-      var sessionId = socket.request.signedCookies['connect.sid'];
+      const sessionId = socket.request.signedCookies['connect.sid'];
 
       // Use the mongoStorage instance to get the Express session information
       mongoStore.get(sessionId, function (err, session) {
@@ -36,10 +35,18 @@ module.exports = function (server, io, mongoStore) {
     });
   });
 
-  // Add an event listener to the 'connection' event
   io.on('connection', function (socket) {
-    // Load the chat controller
-    //require('../app/controllers/chat.server.controller')(io, socket);
     debug('SocketIO client connected')
+
+    ss(socket).on('audio', function (istream) {
+      for (let i in io.sockets.connected) {
+        if (io.sockets.connected[i].id != socket.id) {
+          const outbound = io.sockets.connected[i]
+          const ostream = createStream()
+          ss(outbound).emit('audio', ostream);
+          istream.pipe(ostream);
+        }
+      }
+    });
   });
 };
